cea9d8b5e3edf5825edbf400422840ddd453c450
RichardY42 <RichardY42@nycstudents.net>
2025-10-28 13:15:19 -0400

    progress

--- pet.py
+++ pet.py
@@ -1,1 +1,9 @@
-class pet(,)
+class pet:
+    def __init__(self, name, age, toyinv, tricks, energy, hunger):
+        self.name=name
+        self.age=age
+        self.toyinv=toyinv
+        self.tricks=tricks
+        self.energy=energy
+        self.hunger=hunger
+    def tr(self,tricks)
