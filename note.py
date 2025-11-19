a5d7d2919c262bfc1d3fb799a66ada95ab8f2056
RichardY42 <RichardY42@nycstudents.net>
2025-11-19 10:16:40 -0500

    paswor

--- note.py
+++ note.py
@@ -1,8 +1,39 @@
+
+# Word Problem: The School Portal Login System
+# Your school is creating a new online portal for students to sign up for accounts. The login system needs a function that accepts two pieces of information from the user:
+# Their email address
+# Their password
+# Before creating the new account, the function must verify that the email and password follow school rules:
+# The email must be a string and must contain an "@" symbol.
+# The password must also be a string.
+# The password must be at least 8 characters long.
+# The password must include at least one number.
+# The password must include at least one uppercase letter.
+# If ANY of these rules are broken, the function should return an error message explaining what went wrong.
+# If EVERYTHING is good, the function should return a dictionary that represents the newly created user.
+
+
 def isvalid(email, paswor):
+    if not isinstance(email,str) or not isinstance(paswor,str):
+        return "the email and password is not valid, must be string"
     if "@" not in email:
-        return "Not valid email"
+        return "Not valid email, nust have @ symbol"
+    amount=0
+    for i in range(len(paswor)):
+        amount+=1
+    print(amount)
+    if amount<8:
+        return "your password must be at least eight characters long"
+    number=False
+    if not any(i.isdigit() for i in paswor):
+        return "your password must have a number"
+    if not any(i.isupper() for i in paswor)
+
+
+
+
+
     #print("test")
-    if not isinstance(email,str):
-        return "the email is not valid"
+    
     return {"email":email, "password":paswor}
-print(isvalid("test123@gamer.com",1))
+print(isvalid("test123@gamer.com","qwertyuiop123"))
