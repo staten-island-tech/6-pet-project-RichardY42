8c2f47667b466f9c69b025be77de07d478e64118
RichardY42 <RichardY42@nycstudents.net>
2026-01-14 09:49:04 -0500

    working on oop but here cause nathan working in character.py and we dont want conflicts

--- guard.py
+++ guard.py
@@ -1,35 +1,4 @@
 
-# Word Problem: The School Portal Login System
-# Your school is creating a new online portal for students to sign up for accounts. The login system needs a function that accepts two pieces of information from the user:
-# Their email address
-# Their password
-# Before creating the new account, the function must verify that the email and password follow school rules:
-# The email must be a string and must contain an "@" symbol.
-# The password must also be a string.
-# The password must be at least 8 characters long.
-# The password must include at least one number.
-# The password must include at least one uppercase letter.
-# If ANY of these rules are broken, the function should return an error message explaining what went wrong.
-# If EVERYTHING is good, the function should return a dictionary that represents the newly created user.
-
-
-""" def isvalid(email, paswor):
-    if not isinstance(email,str) or not isinstance(paswor,str):
-        return "the email and password is not valid, must be string"
-    if "@" not in email:
-        return "Not valid email, nust have @ symbol"
-    if len(paswor)<8:
-        return "your password must be at least eight characters long"
-    if not any(i.isdigit() for i in paswor):
-        return "your password must have a number"
-    if not any(i.isupper() for i in paswor):
-        return "your password must have a uppercase letter"
-
-
-    
-    return {"email":email, "password":paswor}
-print(isvalid("test123@gamer.com","F2d8layf")) """
-
 
 weapontype=[
     {
@@ -154,8 +123,7 @@ class Class:
             if i['genre'] == "weapon":
                 print(f"{index}. {i['name']}")
         choice=input("type the name: ").lower()
-            #ayaan proof this later
-            #for y in range(len(self.inv)):
+            
         
         if any(choice == i['name'] for i in self.equipped):
             print("you have already equipped that")
@@ -168,12 +136,18 @@ class Class:
 
         for x in weapontype:
             if choice==x['name'] and x['genre']=="weapon":
-                if len(self.equipped) >
+                
+                if len(self.equipped) == 1:
+                    self.phydmg -= x['phydmg']
+                    self.rngdmg-=x['rngdmg']
+                    self.mgcdmg-=x['mgcdmg']
+                    self.equipped.pop(0)
                 self.equipped.append(x)
                 print(self.phydmg,self.rngdmg,self.mgcdmg)
-                self.phydmg + x['phydmg']
-                self.rngdmg+x['rngdmg']
-                self.mgcdmg+x['mgcdmg']
+                print(self.inv)
+                self.phydmg += x['phydmg']
+                self.rngdmg+=x['rngdmg']
+                self.mgcdmg+=x['mgcdmg']
                 print(self.phydmg,self.rngdmg,self.mgcdmg)
         print(f"currently equipped: {self.equipped}")
 
